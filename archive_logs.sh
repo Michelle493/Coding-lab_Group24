2d69af7b1f2d8df03c100f20240b7cf9fdcfa1d4
erichategekimana <h.eric@alustudent.com>
2025-06-04 07:01:29 -0700

    archive_logs updated

--- archive_logs.sh
+++ archive_logs.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
-#create a path variable
+
+
+#create a path variables
 ACTIVE_LOGS="hospital_data/active_logs"
 ARCHIVED_LOGS="hospital_data/archived_logs"
-#prompt user to select file
-echo "select log to archive: "
-echo "1) Heart Rate"
-echo "2) Temperature"
-echo "3) Water Usage"
-read -p "Enter you choice(1-3)" user_input
 
-#
+
+
+#this commands, will prompt a user to select file
+echo "Select log file to archive: "
+echo "1) Heart Rate "
+echo "2) Temperature "
+echo "3) Water Usage "
+read -p "Enter you choice(1/2/3): " user_input
+
+
+
+#here, program will check if user input match the exist file
 if [ "$user_input" -eq 1 ]; then
-	log=$ACTIVE_LOGS/heart_rate_log.log
-	archived=$ARCHIVED_LOGS/heart_rate/heart_rate_"$(date +'Y%-%m-%d_%h:%m:%s')".log
+	log_file=$ACTIVE_LOGS/heart_rate_log.log
+	archived_file=$ARCHIVED_LOGS/heart_rate/heart_rate_"$(date +'Y%-%m-%d_%h:%m:%s')".log
 
 elif [ "$user_input" -eq 2 ]; then
-        log=$ACTIVE_LOGS/temperature_log.log
-        archived=$ARCHIVED_LOGS/temperature/temperature__"$(date +'Y%-%m-%d_%h:%m:%s')".log
+        log_file=$ACTIVE_LOGS/temperature_log.log
+        archived_file=$ARCHIVED_LOGS/temperature/temperature_"$(date +'Y%-%m-%d_%h:%m:%s')".log
 
 elif [ "$user_input" -eq 3 ]; then
-        log=$ACTIVE_LOGS/water_usage_log.log
-        archived=$ARCHIVED_LOGS/water_usage/water_usage_"$(date +'Y%-%m-%d_%h:%m:%s')".log
+        log_file=$ACTIVE_LOGS/water_usage_log.log
+        archived_file=$ARCHIVED_LOGS/water_usage/water_usage_"$(date +'Y%-%m-%d_%h:%m:%s')".log
+#if user intered something apart from expected(1,2, or 3), then it will print error and exit the program
 else
-	echo "Invalid input"
+	echo "Invalid input! Try again."
 	exit 1
 fi
 
-#then move the file to archive directory
-echo "Archiving $log"
 
-if [ -f "$log" ]; then
-    mkdir -p "$(dirname "$archived")"
-    mv "$log" "$archived"
+#this is message to tell user that $log is being archied
+echo "Archiving "$log_file"..."
+#then move the file to archive directory if exist. if it don't, creat it first and proced
+if [ -f "$log_file" ]; then
+    mkdir -p "$(dirname "$archived_file")"
+    mv "$log_file" "$archived_file"
     touch "$log"
-    echo "Successfully archived to $archived"
+    echo "Successfully archived to $archived_file"
+#if $log file not found, print an error and then exit the program
 else
-    echo "File $log not found!"
+    echo "File $log_file not found!"
     exit 2
 fi
