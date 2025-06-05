a5406f513ba4c13dbea658cc511eb8104e4c2737
erichategekimana <h.eric@alustudent.com>
2025-06-05 09:01:10 -0700

    path debugging

--- archive_logs.sh
+++ archive_logs.sh
@@ -23,16 +23,16 @@ read -p "Enter you choice(1/2/3): " user_input
 
 #here, program will check if user input match the exist file
 if [ "$user_input" -eq 1 ]; then
-	log_file=$ACTIVE_LOGS/heart_rate_log.log
-	archived_file=$ARCHIVED_LOGS/heart_rate/heart_rate_"$TIMESTAMP".log
+	log_file=$ACTIVE_LOGS/heart_rate.log
+	archived_file=$ARCHIVED_LOGS/heart_data_archive/heart_rate_"$TIMESTAMP".log
 
 elif [ "$user_input" -eq 2 ]; then
-        log_file=$ACTIVE_LOGS/temperature_log.log
-        archived_file=$ARCHIVED_LOGS/temperature/temperature_"$TIMESTAMP".log
+        log_file=$ACTIVE_LOGS/temperature.log
+        archived_file=$ARCHIVED_LOGS/temperature_data_archive/temperature_"$TIMESTAMP".log
 
 elif [ "$user_input" -eq 3 ]; then
-        log_file=$ACTIVE_LOGS/water_usage_log.log
-        archived_file=$ARCHIVED_LOGS/water_usage/water_usage_"$TIMESTAMP".log
+        log_file=$ACTIVE_LOGS/water_usage.log
+        archived_file=$ARCHIVED_LOGS/water_usage_data_archive/water_usage_"$TIMESTAMP".log
 #if user intered something apart from expected(1,2, or 3), then it will print error and exit the program
 else
 	echo "Invalid input! Try again."
